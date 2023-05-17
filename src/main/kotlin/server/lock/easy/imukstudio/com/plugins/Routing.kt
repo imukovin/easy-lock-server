17333ffbf9e022767e9182082f124520f8d242c8
a.mukovin <a.mukovin@intabia.ru>
2023-05-17 19:33:09 +0700

    Describe post request for image data.

--- src/main/kotlin/server/lock/easy/imukstudio/com/plugins/Routing.kt
+++ src/main/kotlin/server/lock/easy/imukstudio/com/plugins/Routing.kt
@@ -3,13 +3,21 @@ package server.lock.easy.imukstudio.com.plugins
 import io.ktor.server.routing.routing
 import io.ktor.server.application.Application
 import io.ktor.server.application.call
+import io.ktor.server.request.receiveParameters
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.get
+import io.ktor.server.routing.post
 
 fun Application.configureRouting() {
     routing {
         get("/") {
             call.respondText("Hello World!")
+        }
+        post("/image") {
+            val parameters = call.receiveParameters()
+            val image = parameters["image"].toString()
+
+            call.respondText("Received: $image")
         }
     }
 }
